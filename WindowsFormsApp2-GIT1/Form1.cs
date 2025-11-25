b97ff80d35a7419018b702ab6d6c023e5ead8c65
rcanelas <50590195+rcanelas@users.noreply.github.com>
2025-11-25 21:33:36 +0000

    comite inicial

--- WindowsFormsApp2-GIT1/Form1.cs
+++ WindowsFormsApp2-GIT1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp2_GIT1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- WindowsFormsApp2-GIT1/Program.cs
+++ WindowsFormsApp2-GIT1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp2_GIT1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); //teste
         }
     }
 }
